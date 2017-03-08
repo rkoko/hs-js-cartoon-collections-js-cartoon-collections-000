1ea7f28c6fdb1f01528dc0d55a9fa6f138207509
Rinda Ko <rinda.ko@gmail.com>
2017-03-08 19:35:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves){
+  var array = [];
+  for (var i=0; i<dwarves.length; i++){
+    var count = i+1;
+    array.push(`${count}. ${dwarves[i]}`);
+  }
+  var string = array.join(" ")+" ";
+  return string;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(veggies){
+  var string = veggies.toString();
+  var caps = string.toUpperCase();
+  var newArray = caps.split(',');
+  var array=[];
+  for (var i=0; i<newArray.length; i++){
+    array.push(`${newArray[i]}!`)
+  }
+  return array;
 }
 
 function longPlaneteerCalls(words) {
+  for(var i=0; i<words.length; i++){
+    if(words[i].length>4){
+      return true
+    }else{
+      return false
+    }
+  }
 }
 
-function findTheCheese (foods) {
-}
+function findTheCheese(foods){
+  for(var i =0; i<foods.length; i++){
+    if(foods[i]==="cheddar"|| foods[i]==="gouda"||foods[i]==="camembert"){
+      return foods[i]
+    } }
+    return ("no cheese!")}
+  
